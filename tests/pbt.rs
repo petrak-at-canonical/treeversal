934d617c504d25f41a82a0b69c7d37cb687855e9
Petrichor Park <petrichor.park@canonical.com>
2026-06-01 09:13:23 -0500

    make each run of hammer_tree test do different things

--- tests/pbt.rs
+++ tests/pbt.rs
@@ -21,11 +21,11 @@ fn random_interaction(rand: &mut Rng) -> TreeInteraction {
 // Tree interactions can return errors but shouldn't panic
 #[test]
 fn hammer_tree_with_random_interactions() {
-  for _ in 0..100 {
+  for i in 0..100 {
     let tree = helpers::example_tree();
     let mut interactor = TreeInteractor::new(tree);
 
-    let mut rng = Rng::with_seed(0o7604);
+    let mut rng = Rng::with_seed(0o7604 + i);
     for _ in 0..10000 {
       let interaction = random_interaction(&mut rng);
       let _ignore = interactor.interact(interaction);
